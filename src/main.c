0e034aef494d31289b1dd47cdce6fedc2c8d4020
flippyshoe <flippyshoe@debian-desktop-snes.lan>
2024-05-21 18:13:51 +0300

    implemented a deltatime, movement is now not janky

--- src/main.c
+++ src/main.c
@@ -17,32 +17,41 @@
  * - refactor the codebase :3
  */
 
+#define MOVESPEED 75
+#define JUMPSPEED 150
+#define GRAVITY   50
+
 #define WIN_WIDTH 1600
 #define WIN_HEIGHT 900
+
+#define DEBUGSPEED 16000
 
 #define MOVELEFT  0b00000001
 #define MOVERIGHT 0b00000010
 #define JUMP      0b00000100
 
-#define SU        25 // pixels to define a SIZE UNIT (my answer of a measurement unit i can convert to pixels)
+#define SU        10 // pixels to define a SIZE UNIT (my answer of a measurement unit i can convert to pixels)
 
 /* structs */
 typedef struct {
-  float x, y;
+  double x, y;
 } Vector2;
 
 typedef struct {
   SDL_Rect matter;
   Vector2 velocity;
+  Vector2 position;
   Vector2 acceleration;
 } Entity;
 
 /* bad practice but whatever */
+uint64_t lastframetime = 0;
+
 SDL_Window *win;
 SDL_Renderer *ren;
 Entity player;
 uint8_t moveflags;
-const int GRAV = 1*SU;
+const int GRAV = GRAVITY*SU;
 
 void startApp(){
   win = SDL_CreateWindow(
@@ -74,32 +83,41 @@ void exitApp(){
 
 void startPlayer(){
   memset(&player, 0, sizeof(Entity));
+  player.position.x = WIN_WIDTH/2;
+  player.position.y = WIN_HEIGHT/2;
   player.matter.x = WIN_WIDTH/2;
   player.matter.y = WIN_HEIGHT/2;
-  player.matter.w = 75;
-  player.matter.h = 100;
+  player.matter.w = 7*SU;
+  player.matter.h = 10*SU;
 }
 
-void movePlayer(){
+void movePlayer(double dt){
   // set gravity
-  player.acceleration.y = GRAV;
+  player.acceleration.y = GRAV*SU;
   
   /* get user input */
   player.velocity.x = 0;
-  if(moveflags & MOVELEFT)        { player.velocity.x = -1*SU; }
-  else if (moveflags & MOVERIGHT) { player.velocity.x =  1*SU; }
+  if(moveflags & MOVELEFT)        { player.velocity.x = -1*MOVESPEED*SU; }
+  else if (moveflags & MOVERIGHT) { player.velocity.x =  1*MOVESPEED*SU; }
 
-  if(moveflags & JUMP)            { player.velocity.y = -5*SU; }
-
-  printf("%3d\n", player.acceleration.y);
+  if(moveflags & JUMP)            { player.velocity.y = -1*JUMPSPEED*SU; }
   
   /* apply acceleration */
-  player.velocity.x += player.acceleration.x;
-  player.velocity.y += player.acceleration.y;
+  player.velocity.x += player.acceleration.x*dt;
+  player.velocity.y += player.acceleration.y*dt;
   
   /* apply velocity */
-  player.matter.x += player.velocity.x;
-  player.matter.y += player.velocity.y;
+  player.position.x += player.velocity.x*dt;
+  player.position.y += player.velocity.y*dt;
+
+  player.matter.x = player.position.x;
+  player.matter.y = player.position.y;
+
+  printf("ACC = (%lf,%lf)\nVEL = (%lf,%lf)\nPOS = (%lf,%lf)\n=\n",
+	 player.acceleration.x, player.acceleration.y,
+	 player.velocity.x, player.velocity.y,
+	 player.position.x, player.position.y
+    );
 }
 
 /* until i get collision detection :) */
@@ -107,12 +125,13 @@ void setEntityWithinBounds(Entity *e){
   //e->acceleration.x = (e->matter.x+e->matter.w > WIN_WIDTH) ? e->acceleration.x-GRAV : e->acceleration.x;
 
   if(e->matter.y+e->matter.h > WIN_HEIGHT){
-    e->matter.y=WIN_HEIGHT-e->matter.h;
+    e->position.y=WIN_HEIGHT-e->matter.h;
     e->velocity.y = 0;
   }
 }
 
 int main(){
+  
   /* initialise sdl, set exit condition */
   ASSERT(
     SDL_Init(SDL_INIT_VIDEO) == 0,
@@ -133,6 +152,9 @@ int main(){
   const uint8_t *ks;
   
   while(run){
+    double dt = (double)(SDL_GetTicks64() - lastframetime) / 1000.0; // now it works??
+    lastframetime = SDL_GetTicks64();
+    
     // reset the move flags
     moveflags = 0;
     
@@ -149,8 +171,10 @@ int main(){
     ks = SDL_GetKeyboardState(NULL);
     if(ks[SDL_SCANCODE_A])       {moveflags += MOVELEFT;}
     else if (ks[SDL_SCANCODE_D]) {moveflags += MOVERIGHT;}
+
+    printf("%d\n", moveflags);
     
-    movePlayer();
+    movePlayer(dt);
     setEntityWithinBounds(&player);
 
     /* draw the scene (player is the red rectangle) */
@@ -162,8 +186,7 @@ int main(){
     
     SDL_RenderPresent(ren);
 
-    /* hard-coded delay to mimic 60 fps? HELL YEAH! */
-    usleep(64000);
+    usleep(DEBUGSPEED);
   }
   
   exit(0);
